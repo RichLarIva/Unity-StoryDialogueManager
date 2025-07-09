02e1d4962b1fe76ea6e76ce9a7cdf5cd38865ffd
Richard <54506990+RichLarIva@users.noreply.github.com>
2025-07-09 12:38:00 +0200

    Enum for dialogue types created and lines handling

---
+++ DialogueType.cs
@@ -0,0 +1,10 @@
+public enum DialogueType
+{
+    // Represents different types of dialogues in the game. All are currently just examples.
+    Greeting,
+    StoreTalk,
+    QuestStart,
+    QuestEnd,
+    QuestProgress,
+    CharacterInteraction
+}
